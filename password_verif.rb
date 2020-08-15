722ae4a7c711c250926269da8af4b88673261a0c
Christabel Sebastian <christabel.escarez@gmail.com>
2020-08-15 01:13:36 -0700

    commented out old code from unrelated project; began loop structure for incrementing through array

--- password_verif.rb
+++ password_verif.rb
@@ -10,37 +10,42 @@ end
 
 def check_input(user_input)
   while !password
-    user_input[] = gets.chomp
-    if (user_input[].length < 8)
+    user_input = gets.chomp
+    if (user_input.length < 8)
       invalid_input_message("Password length too short.")      
-    elsif (user_input == user_input.map(&:upcase) || user_input[] == user_input[].lowercase)
+    elsif ((user_input == user_input.map(&:upcase) || user_input == user_input.map(&:lowercase)))
       invalid_input_message("Password does not contain both uppercase and lowercase letters.")      
     elsif
-      #
+      user_input.each do |char|
+        until (char == @ || char == % || char == * || char == !)
+          
+        end
+        password_upcase[user_input.index(char)] = char.uppercase
+        password_downcase[user_input.index(char)] = char.lowercase
+      end
     end
   end
   return user_input
 end
   
-#MAIN
-puts "Please enter a password that is at least 8 characters long and contains at least: one uppercase letter, one lowercase letter, one number, and one special character (@, %, *, or !)."
-total, number1, number2 = [0] * 3
+# #MAIN
+# puts "Please enter a password that is at least 8 characters long and contains at least: one uppercase letter, one lowercase letter, one number, and one special character (@, %, *, or !)."
 
-while (number1 >= 0 && number2 >= 0) 
-  number2 = number1 + 1 #offset number1 and number2 to enter below loop
-  until number1 == number2
-    puts "Please enter a number. Enter a negative number to quit."
-    number1 = check_input(gets.chomp)
-    puts "Please enter the number again to confirm entry."
-    number2 = check_input(gets.chomp) 
-    if number1 != number2
-      puts "#{number1} does not match #{number2}, please try again."
-    end
-  end
-    total += number1
-    puts "the current total is #{total}"
-end
+# while (password1 >= 0 && password2 >= 0) 
+#   password2 = password1 + 1 #offset password1 and password2 to enter below loop
+#   until password1 == password2
+#     puts "Please enter a number. Enter a negative number to quit."
+#     password1 = check_input(gets.chomp)
+#     puts "Please enter the number again to confirm entry."
+#     password2 = check_input(gets.chomp) 
+#     if password1 != password2
+#       puts "#{password1} does not match #{password2}, please try again."
+#     end
+#   end
+#     total += password1
+#     puts "the current total is #{total}"
+# end
 
-puts "Result: #{total}"
+# puts "Result: #{total}"
   
 
