bfba4e40c51d3011eb624c7f3925e08eec7467c9
cbel-mac-vm <cbel-mac-vm@cbel-mac-vms-iMac.local>
2020-08-17 20:36:13 -0700

    added check for invalid characters.. I think... the nested if statement blob for invalid inputs is getting larger... I think it is becoming sentient... end of log

--- password_verif.rb
+++ password_verif.rb
@@ -11,7 +11,8 @@ requirements_msg = "Please re-enter a password that is at least 8 characters lon
 not_long = "length is too short."
 no_special_characters = "does not contain any special characters"
 no_numbers = "does not contain any numbers"
-no_different_cases = " does not contain both lowercase and uppercase letters"
+no_different_cases = "does not contain both lowercase and uppercase letters"
+invalid_characters = "contains invalid character(s)"
 #Functions
 def invalid_input_message(reason)
   puts "Invalid input. Password #{reason}. #{@requirements_msg}"
@@ -22,11 +23,13 @@ puts "#{requirements_msg}"
 password = false
 until password
   password_length = special_char_found = number_found = different_cases = false
+  all_valid_characters = true
   uppercase = lowercase = false
-  input_array = Array.new
 
+  input_array = Array.new
   input_string = gets.chomp
   input_array = input_string.split('')
+
   input_array.each do |char|
     if char.to_s == char.to_i.to_s
       number_found = true
@@ -36,8 +39,10 @@ until password
       else
         if (('A'..'Z').include?(char))
           uppercase = true
-        else (('a'..'z').include?(char))
+        elsif (('a'..'z').include?(char))
           lowercase = true
+        else
+          all_valid_characters = false
         end
       end
     end
@@ -52,63 +57,157 @@ until password
   end
 
 #SO UGLY... but it works. Don't look this code block in the eye!
-  if !password_length 
-    if !special_char_found 
-      if !number_found
-        if !different_cases
-          invalid_input_message(not_long + no_special_characters + no_numbers + no_different_cases)      
+  if !all_valid_characters
+    if !password_length 
+      if !special_char_found 
+        if !number_found
+          if !different_cases
+            invalid_input_message(not_long + ", " + no_special_characters + ", " + no_numbers + ", " + no_different_cases + ", " + invalid_characters)
+          else
+            invalid_input_message(not_long + ", " + no_special_characters + ", " + no_numbers + ", " + invalid_characters)      
+          end
         else
-          invalid_input_message(not_long + no_special_characters + no_numbers)      
+          if !different_cases
+            invalid_input_message(not_long + ", " + no_special_characters + ", " + no_different_cases + ", " + invalid_characters)      
+          else
+            invalid_input_message(not_long + ", " + no_special_characters + ", " + invalid_characters)      
+          end
         end
       else
-        if !different_cases
-          invalid_input_message(not_long + no_special_characters + no_different_cases)      
+        if !number_found
+          if !different_cases
+            invalid_input_message(not_long + ", " + no_numbers + ", " + no_different_cases + ", " + invalid_characters)      
+          else
+            invalid_input_message(not_long + ", " + no_numbers + ", " + invalid_characters)      
+          end
         else
-          invalid_input_message(not_long + no_special_characters)      
+          if !different_cases
+            invalid_input_message(not_long + ", " + no_different_cases + ", " + invalid_characters)      
+          else
+            invalid_input_message(not_long + ", " + invalid_characters)      
+          end
         end
       end
     else
-      if !number_found
-        if !different_cases
-          invalid_input_message(not_long + no_numbers + no_different_cases)      
+      if !special_char_found 
+        if !number_found
+          if !different_cases
+            invalid_input_message(no_special_characters + ", " + no_numbers + ", " + no_different_cases + ", " + invalid_characters)      
+          else
+            invalid_input_message(no_special_characters + ", " + no_numbers + ", " + invalid_characters)      
+          end
         else
-          invalid_input_message(not_long + no_numbers)      
+          if !different_cases
+            invalid_input_message(no_special_characters + ", " + no_different_cases + ", " + invalid_characters)   
+          else
+            invalid_input_message(no_special_characters + ", " + invalid_characters)      
+          end
         end
       else
-        if !different_cases
-          invalid_input_message(not_long + no_different_cases)      
+        if !number_found
+          if !different_cases
+            invalid_input_message(no_numbers + ", " + no_different_cases + ", " + invalid_characters)      
+          else
+            invalid_input_message(no_numbers + ", " + invalid_characters)      
+          end
         else
-          invalid_input_message(not_long)      
+          if !different_cases
+            invalid_input_message(no_different_cases + ", " + invalid_characters)      
+          else
+            invalid_input_message(invalid_characters)      
+          end
         end
       end
     end
-  else
-    if !special_char_found 
-      if !number_found
-        if !different_cases
-          invalid_input_message(no_special_characters + no_numbers + no_different_cases)      
+    else
+      if !special_char_found 
+        if !number_found
+          if !different_cases
+            invalid_input_message(not_long + ", " + no_special_characters + ", " + no_numbers + ", " + no_different_cases)      
+          else
+            invalid_input_message(not_long + ", " + no_special_characters + ", " + no_numbers)      
+          end
         else
-          invalid_input_message(no_special_characters + no_numbers)      
+          if !different_cases
+            invalid_input_message(not_long + ", " + no_special_characters + ", " + no_different_cases)      
+          else
+            invalid_input_message(not_long + ", " + no_special_characters)      
+          end
         end
       else
-        if !different_cases
-          invalid_input_message(no_special_characters + no_different_cases)      
+        if !number_found
+          if !different_cases
+            invalid_input_message(not_long + ", " + no_numbers + ", " + no_different_cases)      
+          else
+            invalid_input_message(not_long + ", " + no_numbers)      
+          end
         else
-          invalid_input_message(no_special_characters)      
+          if !different_cases
+            invalid_input_message(not_long + ", " + no_different_cases)      
+          else
+            invalid_input_message(not_long)      
+          end
+        end
+      end
+    if !password_length 
+      if !special_char_found 
+        if !number_found
+          if !different_cases
+            invalid_input_message(not_long + ", " + no_special_characters + ", " + no_numbers + ", " + no_different_cases + ", " + invalid_characters)
+          else
+            invalid_input_message(not_long + ", " + no_special_characters + ", " + no_numbers + ", " + invalid_characters)      
+          end
+        else
+          if !different_cases
+            invalid_input_message(not_long + ", " + no_special_characters + ", " + no_different_cases + ", " + invalid_characters)      
+          else
+            invalid_input_message(not_long + ", " + no_special_characters + ", " + invalid_characters)      
+          end
+        end
+      else
+        if !number_found
+          if !different_cases
+            invalid_input_message(not_long + ", " + no_numbers + ", " + no_different_cases + ", " + invalid_characters)      
+          else
+            invalid_input_message(not_long + ", " + no_numbers + ", " + invalid_characters)      
+          end
+        else
+          if !different_cases
+            invalid_input_message(not_long + ", " + no_different_cases + ", " + invalid_characters)      
+          else
+            invalid_input_message(not_long + ", " + invalid_characters)      
+          end
         end
       end
     else
-      if !number_found
-        if !different_cases
-          invalid_input_message(no_numbers + no_different_cases)      
+      if !special_char_found 
+        if !number_found
+          if !different_cases
+            invalid_input_message(no_special_characters + ", " + no_numbers + ", " + no_different_cases)      
+          else
+            invalid_input_message(no_special_characters + ", " + no_numbers)      
+          end
         else
-          invalid_input_message(no_numbers)      
+          if !different_cases
+            invalid_input_message(no_special_characters + ", " + no_different_cases)      
+          else
+            invalid_input_message(no_special_characters)      
+          end
         end
       else
-        if !different_cases
-          invalid_input_message(no_different_cases)      
+        if !number_found
+          if !different_cases
+            invalid_input_message(no_numbers + ", " + no_different_cases)      
+          else
+            invalid_input_message(no_numbers)      
+          end
         else
-          password = input_array
+          if !different_cases
+            invalid_input_message(no_different_cases)      
+          else
+            password = input_array
+            puts "password"
+          end
         end
       end
     end
@@ -118,7 +217,7 @@ end
 puts "Your password has been accepted. Goodbye."
 
 # while (password1 >= 0 && password2 >= 0) 
-#   password2 = password1 + 1 #offset password1 and password2 to enter below loop
+#   password2 = password1 + ", " + 1 #offset password1 and password2 to enter below loop
 #   until password1 == password2
 #     puts "Please enter a number. Enter a negative number to quit."
 #     password1 = check_input(gets.chomp)
@@ -128,7 +227,7 @@ puts "Your password has been accepted. Goodbye."
 #       puts "#{password1} does not match #{password2}, please try again."
 #     end
 #   end
-#     total += password1
+#     total + ", " += password1
 #     puts "the current total is #{total}"
 # end
 
